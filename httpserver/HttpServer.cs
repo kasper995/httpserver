b075e33a61a4475875110db285ada0fdbb8d4bd2
kasper995 <kasper995@hotmail.dk>
2014-09-29 14:27:02 +0200

    cleaned and commented

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -14,64 +14,48 @@ namespace httpserver
 {
     public class HttpServer
     {
-        public static readonly int DefaultPort = 8888;
-        private static readonly string RootCatalog = "C:\\Windows\\Temp";
+        public static readonly int DefaultPort = 8888; // den valgte port
+        private static readonly string RootCatalog = "C:\\Windows\\Temp"; //rootcaltalog som angiver stien til filen
 
 
         public void StartServer()
         {
             while (true)
             {
-
-
-                TcpListener serverSocket = new TcpListener(DefaultPort);
-                serverSocket.Start();
-
-
-                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-                Stream ns = connectionSocket.GetStream();
-                StreamReader sr = new StreamReader(ns);
-                
-                StreamWriter sw = new StreamWriter(ns);
+                TcpListener serverSocket = new TcpListener(DefaultPort); // laver serveren
+                serverSocket.Start(); //starter serveren
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient(); //sætter sockets til at acceptere clienten
+                Stream ns = connectionSocket.GetStream(); //laver en stream
+                StreamReader sr = new StreamReader(ns); // laver en streamreader der hedder sr
+                StreamWriter sw = new StreamWriter(ns); // laver en streamwriter der hedder sw
 
                 sw.AutoFlush = true; // enable automatic flushing
-                
+
                 try
                 {
-
-                    string message = sr.ReadLine();
-                    string[] words = message.Split(' ');
-                    string doh = words[1].Replace("/","\\");
-                  
-                    string doh2 = RootCatalog + doh;
-
-
-                    if (File.Exists(doh2))
+                    string message = sr.ReadLine(); // læser htlm requesten
+                    string[] words = message.Split(' '); //laver et array med ordene fra message så hvert ord 
+                    string doh = words[1].Replace("/", "\\"); // bytter alle / ud med \, 
+                    string doh2 = RootCatalog + doh; // tager rootcatalog og dog samler dem i en ny string doh2
+                    
+                    if (File.Exists(doh2)) // checker om filen findes
                     {
-                        string ftext = File.ReadAllText(doh2);
-                        sw.Write(ftext);
+                        string ftext = File.ReadAllText(doh2); // samler filnen i en string der hedder ftext hvis den kan læses
+                        sw.Write(ftext); // sender stringen til browseren så den kan læses
                     }
 
                     else
                     {
-                        sw.Write("HTTP/1.0 200 Ok\r\n");
-                        sw.Write("\r\n");
-                        sw.Write("the requested file or homepagde do not exist");
-                        sw.Write(" {0}", doh2);
-
-
-
+                        sw.Write("HTTP/1.0 200 Ok\r\n"); // sender header til browseren
+                        sw.Write("\r\n"); // lineskift så den ved det er body der kommer som det næste
+                        sw.Write("the requested file or homepagde {0} do not exist", doh2); // sender svar tilbage til browseren om at serveren ikke har det de søger
                     }
-                    
-                    //sw.Write("you have requested file {0}", doh2);
-                    
                 }
-                    
                 finally
                 {
-                    ns.Close();
-                    connectionSocket.Close();
-                    serverSocket.Stop();
+                    ns.Close(); // lukker streamen ns ned
+                    connectionSocket.Close(); // lukker connectionsocket ned
+                    serverSocket.Stop(); // lukker serversocket ned
                 }
             }
         }
