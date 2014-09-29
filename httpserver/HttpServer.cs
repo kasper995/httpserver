0c38e86732d3b3a8045130608bb605221be4535c
kasper995 <kasper995@hotmail.dk>
2014-09-29 12:09:28 +0200

    try finally

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -14,27 +14,38 @@ namespace httpserver
         public static readonly int DefaultPort = 8888;
         public void StartServer()
         {
+            while (true)
+            {
+                
+               
             TcpListener serverSocket = new TcpListener(DefaultPort);
             serverSocket.Start();
+           
 
-            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-            
+               TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                    Stream ns = connectionSocket.GetStream();
+                    StreamReader sr = new StreamReader(ns);
+                    StreamWriter sw = new StreamWriter(ns);
 
-            Stream ns = connectionSocket.GetStream();
-            StreamReader sr = new StreamReader(ns);
-            StreamWriter sw = new StreamWriter(ns);
-            
-            sw.AutoFlush = true; // enable automatic flushing
+                    sw.AutoFlush = true; // enable automatic flushing
+                try
+                {
 
-            string message = sr.ReadLine();
-            Console.WriteLine("hello");
-            sw.Write("HTTP/1.0 200 Ok\r\n");
-            sw.Write("\r\n");
-            sw.Write("hello");
-            ns.Close();
-            connectionSocket.Close();
-            serverSocket.Stop();
 
+                    string message = sr.ReadLine();
+                    
+                    sw.Write("HTTP/1.0 200 Ok\r\n");
+                    sw.Write("\r\n");
+                    sw.Write("hello world");
+                }
+                finally
+                {
+                    ns.Close();
+                    connectionSocket.Close();
+                    serverSocket.Stop();
+                }
+            }
+            }
         }
     }
-}
+
