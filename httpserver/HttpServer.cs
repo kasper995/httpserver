360bf10edf8d4fb81f4e87c7b40a2c25c4c0c71a
kasper995 <kasper995@hotmail.dk>
2014-09-29 14:50:27 +0200

    ss

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 
 
+
 namespace httpserver
 {
     public class HttpServer
@@ -17,7 +18,7 @@ namespace httpserver
         public static readonly int DefaultPort = 8888; // den valgte port
         private static readonly string RootCatalog = "C:\\Windows\\Temp"; //rootcaltalog som angiver stien til filen
 
-
+       
         public void StartServer()
         {
             while (true)
@@ -37,16 +38,22 @@ namespace httpserver
                     string[] words = message.Split(' '); //laver et array med ordene fra message så hvert ord 
                     string doh = words[1].Replace("/", "\\"); // bytter alle / ud med \, 
                     string doh2 = RootCatalog + doh; // tager rootcatalog og dog samler dem i en ny string doh2
-                    
+                    if (doh2 == RootCatalog)
+                    {
+                        sw.Write("HTTP/1.0 404 not found \r\n"); // sender header til browseren
+                        sw.Write("\r\n"); // lineskift så den ved det er body der kommer som det næste
+                        sw.Write("Hello world");
+                    }
                     if (File.Exists(doh2)) // checker om filen findes
                     {
+                        sw.Write("HTTP/1.0 404 not found \r\n"); // sender header til browseren
+                        sw.Write("\r\n"); // lineskift så den ved det er body der kommer som det næste
                         string ftext = File.ReadAllText(doh2); // samler filnen i en string der hedder ftext hvis den kan læses
                         sw.Write(ftext); // sender stringen til browseren så den kan læses
                     }
-
                     else
                     {
-                        sw.Write("HTTP/1.0 200 Ok\r\n"); // sender header til browseren
+                        sw.Write("HTTP/1.0 404 not found \r\n"); // sender header til browseren
                         sw.Write("\r\n"); // lineskift så den ved det er body der kommer som det næste
                         sw.Write("the requested file or homepagde {0} do not exist", doh2); // sender svar tilbage til browseren om at serveren ikke har det de søger
                     }
